b414093b20b355637dc583195237c312f86835f9
adam lamouri <adamlamouri.pro@gmail.com>
2023-10-27 18:51:06 +0200

    CORRECTION MAIL FONCTIONNEL

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,12 @@
 class ContactMailer < ApplicationMailer
-  def formulaire_de_contact(nom, email, message)
+  def formulaire_de_contact(nom, prenom, email, telephone, entreprise, message)
     @nom = nom
     @email = email
     @message = message
+    @prenom = prenom
+    @telephone = telephone
+    @entreprise = entreprise
+
     mail(to: 'adamlamouri.pro@gmail.com', subject: 'Nouveau message du formulaire de contact')
   end
 end
